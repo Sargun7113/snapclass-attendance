14b57775b9101b48bcfb97345164e2a8c39ec3e7
Sargun <sargunpreetkaur73@gmail.com>
2026-08-20 17:43:06 +0530

    name

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,9 +1,17 @@
 import streamlit as st
 
 
-
-
 def footer_home():
+    st.markdown("""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
+            <p style="font-weight:bold; color:white;">
+                Created with ❤️ by Sargun Mehta
+            </p>
+        </div>
+    """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
     st.markdown("""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
             <p style="font-weight:bold; color:black;">
